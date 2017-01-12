d4ac44e881bd8301d796d592b8fb5494d00ea1bd
Jacob Szemanski <jjsze13@gmail.com>
2017-01-11 21:25:55 -0600

    Added photos for venues and artist. Also updated seed file to include images and to correct error where concerts would not have a band

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 
-
+band_images = ["acoustic.jpg", "acoustic_1.jpg", "acoustic_2.jpg", "rock_1.jpg", "rock_2.jpg", "rock_3.jpg", "xenia_1.jpg", "xenia_2.jpg", "trumpet.jpg"]
+venue_images = {"Thalia Hall" => 'thalia_1.jpg', "Schubas" => 'schubas_1.jpg', "Empty Bottle" => 'empty_bottle_1.jpg', "Double Door" => 'double_door_1.jpg', "Lincoln Hall" => 'lincoln_hall_1.jpg', "House of Blues" => 'house_of_blues_1.jpg'}
 
 names = ['Joe', 'Sam', 'Bobby', 'Trevor', 'Jake', 'John', 'Steve']
 
@@ -14,28 +15,33 @@ end
 bands = ['Twin Peaks', 'Lady Lamb', 'Ode', 'Dawes', 'Meat Wave', 'Bleach Party', 'Dowsing', 'Delta Spirit', 'Speedy Ortiz', 'Modern Baseball', 'Local Natives', 'Arcade Fire', 'Free Throw', 'Diners', 'Cake', 'Built to Spill', 'Ben Folds', 'Andrew WK', 'AJJ', 'Tacocat']
 
 bands.each do |band|
-  artist = Band.new(name: band)
+  artist = Band.new(
+    name: band,
+    image: band_images.sample)
   artist.save
 end
 @bands = Band.all
 
-sites = ['Schubas', 'Lincoln Hall', 'Thalia Hall', 'Empty Bottle', 'Double Door', 'House of Blues', ]
+sites = ['Schubas', 'Lincoln Hall', 'Thalia Hall', 'Empty Bottle', 'Double Door', 'House of Blues']
 will_call = ['strict', 'loose']
 sites.each do |site|
   venue = Venue.new(
     name: site,
     will_call_policy: will_call.sample.to_s,
-    address: 'Chicago'
+    address: 'Chicago',
+    image: venue_images[site]
     )
   venue.save
 end
 @venues = Venue.all
 
 dates = ['2017-02-12', '2017-01-21', '2017-02-05', '2017-01-07', '2017-04-20', '2017-03-04', '2017-06-26']
-times = ["6:00pm", '7:00pm', '8:00pm', '9:00pm', '10:00pm']
+doors = ["5:30pm", "6:00pm", "6:30pm", "7:00pm"]
+
+times = ['7:00pm', "7:30pm", '8:00pm', "8:30pm", '9:00pm']
 7.times do
   show = Concert.new(
-    doors: times.sample,
+    doors: doors.sample,
     show: times.sample,
     venue_id: @venues.sample.id,
     date: dates.sample 
@@ -67,24 +73,29 @@ rev = [
   "Love this place! Come here every week!!!",
   "Meh, things are cooler in Brooklyn"
 ]
+
+
 rating = [3, 5, 4, 2, 4, 5, 1, 3]
-rating.each do |rate|
-  review = Review.new(
-    rating: rate,
-    description: rev.sample,
-    venue_id: @venues.sample.id,
-    user_id: @users.sample.id
-    )
-  review.save
+@venues.each do |venue|
+  3.times do
+    review = Review.new(
+      rating: rating.sample,
+      description: rev.sample,
+      venue_id: venue.id,
+      user_id: @users.sample.id
+      )
+    review.save
+  end
 end
 @reviews = Review.all
 
-10.times do 
-  bc = BandConcert.new(
-    band_id: @bands.sample.id,
-    concert_id: @concerts.sample.id
-    )
-  bc.save
+@concerts.each do |concert|
+  3.times do
+    bc = BandConcert.new(
+      concert_id: concert.id,
+      band_id: @bands.sample.id)
+    bc.save
+  end
 end
 
 10.times do
