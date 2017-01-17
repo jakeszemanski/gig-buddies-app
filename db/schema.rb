82ee63c493591044697e24752910bfa6f78ce35e
Jacob Szemanski <jjsze13@gmail.com>
2017-01-16 20:34:42 -0600

    Add description to bands and updated fake data in seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112011053) do
+ActiveRecord::Schema.define(version: 20170117022043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20170112011053) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image"
+    t.text     "description"
   end
 
   create_table "concerts", force: :cascade do |t|
