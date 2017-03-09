c54969aa229c7cf14b66c88106bc76a50f0af552
jakeszemanski <jakeszemanski@gmail.com>
2017-03-09 11:51:39 -0600

    deleted userband migration

--- db/migrate/20170124032851_add_band_idand_user_id_to_user_bands.rb
+++
@@ -1,6 +0,0 @@
-class AddBandIdandUserIdToUserBands < ActiveRecord::Migration[5.0]
-  def change
-    add_column :user_bands, :user_id, :string
-    add_column :user_bands, :band_id, :string
-  end
-end
